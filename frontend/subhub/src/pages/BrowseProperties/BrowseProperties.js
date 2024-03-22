52c2cb99428327122bff29b600ac90cb52d6e4cc
shaaaaame <hanxheng@gmail.com>
2024-03-22 12:41:37 -0400

    completed initial draft of browse

--- frontend/subhub/src/pages/BrowseProperties/BrowseProperties.js
+++ frontend/subhub/src/pages/BrowseProperties/BrowseProperties.js
@@ -1,24 +1,125 @@
-import { Container, Flex, Grid } from "@mantine/core";
-import React from "react";
+import React, { useState } from "react";
+import {
+    Grid,
+    GridItem,
+    Flex,
+    Select,
+    Image,
+    Container,
+    Input,
+    RangeSlider,
+    RangeSliderTrack,
+    RangeSliderFilledTrack,
+    RangeSliderThumb,
+    RangeSliderMark,
+    Text,
+    Button,
+} from "@chakra-ui/react";
+import "./BrowseProperties.css";
+import PropertyCard from "./PropertyCard";
+import ListingDrawer from "./ListingDrawer";
 
 function BrowseProperties() {
+    const [sliderValue, setSliderValue] = useState([1000, 5000]);
+    const [openSide, setOpenSide] = useState(false);
+
     return (
-        <div className="browse">
-            <Grid>
-                <Grid.Col span={3}>
+        <>
+            <Grid
+                h="100vh"
+                templateColumns="repeat(5, 1fr)"
+                className="browse"
+                color={"#357960"}
+                bg="#fdfdfd"
+            >
+                <GridItem colSpan={1} bg="#fdfdfd" margin="20px">
                     <Flex
                         direction={"column"}
-                        justify={"center"}
                         align={"start"}
-                    ></Flex>
-                </Grid.Col>
-                <Grid.Col span={9}>
-                    <Container
-                        style={{ border: "1px solid black" }}
-                    ></Container>
-                </Grid.Col>
+                        justify={"center"}
+                        padding={"20px"}
+                    >
+                        <Flex
+                            justify={"center"}
+                            align={"center"}
+                            width={"100%"}
+                        >
+                            <Image boxSize="100px" src="/logo.svg" alt="logo" />
+                        </Flex>
+                        <Input
+                            placeholder="enter address!"
+                            colorScheme="whiteAlpha"
+                            m="10px"
+                            mt="30px"
+                            w="auto"
+                            h={"50px"}
+                        ></Input>
+                        <Container marginTop="30px" pb="30px" pt="10px">
+                            <Text>Price range</Text>
+                            <RangeSlider
+                                aria-label={["min", "max"]}
+                                min={0}
+                                max={6000}
+                                defaultValue={[sliderValue[0], sliderValue[1]]}
+                                mt="10px"
+                                onChange={(val) => setSliderValue(val)}
+                            >
+                                <RangeSliderMark
+                                    value={sliderValue[0]}
+                                    textAlign="center"
+                                    color="#357960"
+                                    mt="1"
+                                    marginLeft="-5"
+                                    w="12"
+                                >
+                                    ${sliderValue[0]}
+                                </RangeSliderMark>
+                                <RangeSliderMark
+                                    value={sliderValue[1]}
+                                    textAlign="center"
+                                    color="#357960"
+                                    mt="1"
+                                    ml="-5"
+                                    w="12"
+                                >
+                                    ${sliderValue[1]}
+                                </RangeSliderMark>
+                                <RangeSliderTrack>
+                                    <RangeSliderFilledTrack />
+                                </RangeSliderTrack>
+                                <RangeSliderThumb index={0} />
+                                <RangeSliderThumb index={1} />
+                            </RangeSlider>
+                        </Container>
+                        <Container marginTop="30px" pb="30px" pt="10px">
+                            <Text>Number of bedrooms</Text>
+                            <Select mt="10px">
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                                <option value={5}>5</option>
+                                <option value={6}>6</option>
+                            </Select>
+                        </Container>
+                        <Button onClick={() => setOpenSide(!openSide)}>
+                            Open side
+                        </Button>
+                    </Flex>
+                </GridItem>
+                <GridItem
+                    colSpan={4}
+                    bg="#fdfdfd"
+                    border="1px solid #357960"
+                    borderRadius={"30px"}
+                    margin="20px"
+                ></GridItem>
             </Grid>
-        </div>
+            <ListingDrawer
+                isOpen={openSide}
+                onClose={() => setOpenSide(false)}
+            />
+        </>
     );
 }
 
