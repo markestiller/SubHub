814897befd6cb3fc4a78c24b5ac36baf9ed28ef2
shaaaaame <hanxheng@gmail.com>
2024-03-22 10:53:02 -0400

    added browse properties

--- frontend/subhub/src/index.js
+++ frontend/subhub/src/index.js
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import BrowseProperties from "./pages/BrowseProperties/BrowseProperties";
+import { MantineProvider, createTheme } from "@mantine/core";
 
 const router = createBrowserRouter([
     {
@@ -11,9 +12,15 @@ const router = createBrowserRouter([
     },
 ]);
 
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <MantineProvider theme={theme}>
+            <RouterProvider router={router} />
+        </MantineProvider>
     </React.StrictMode>
 );
