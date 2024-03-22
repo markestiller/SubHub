a148feb6682de9c643504ae3bdc40dacb33cbaf4
Dharma <dharma.ong@mail.utoronto.ca>
2024-03-22 11:04:30 -0400

    added filter class + global variables

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,70 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+// OBJECTS
+class Filter {
+  constructor() {
+      // this.location = ...;
+      this.lowerBound = 0;
+      this.upperBound = Number.MAX_SAFE_INTEGER;
+      this.minBedrooms = 0;
+      // this.startDate = Date();
+      // this.endDate = Date();
+  }
+
+  addBedroomFilter(numBedrooms) {
+    this.minBedrooms = numBedrooms;
+  }
+
+  addPriceFilter(lowerBound, upperBound) {
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+  }
+}
+
+
 // GLOBAL VARIABLES
+const properties = [];
+const propertiesFiltered = []; // stores properties with current subset specified
+const filters = []; // will store values from "/filter-property" POST, then be used in "/get-property-subset"
 
 // HELPER FUNCTIONS
 
 // ROUTE HANDLERS
 
+// req.body = {
+
+
+// }
+
 app.post("/create-property", (req, res) => {
   // frontend sends property information to backend
+  
+});
+
+app.post("/filter-property", (req, res) => {
+  // frontend sends filters for properties to backend
+
+  // 
+
 });
 
 app.get("/get-property", (req, res) => {
   // frontend receives one property information from backend
 });
 
-app.get("/get-properties", (req, res) => {
-  // frontend receives all property information from backend
+app.get("/get-property-subset", (req, res) => {
+  // frontend receives one property information from backend
+
+  // apply filters
+
+
+  // send back to front end
+
+});
+
+app.get("/get-property-all", (req, res) => {
+// frontend receives all property information from backend
 });
 
 // start the server
